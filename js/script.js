982ea4f7123aaf8484264c9925728eb7c858ec22
Espen Holm Pedersen <91594315+PederZzen@users.noreply.github.com>
2022-06-04 14:21:08 +0200

    header stuff and scroll stuff

--- js/script.js
+++ js/script.js
@@ -4,14 +4,48 @@ const menuBtn = document.querySelector("#hamburger");
 const menuBtnClose = document.querySelector("#close-menu");
 const menu = document.querySelector("nav");
 
-menuBtn.addEventListener("click", () => {
+function showMenu () {
     menu.classList.add("show-menu");
     menuBtn.style.display = "none";
     menuBtnClose.style.display = "block";
-})
+}
 
-menuBtnClose.addEventListener("click", () => {
+function hideMenu () {
     menu.classList.remove("show-menu");
     menuBtn.style.display = "block";
     menuBtnClose.style.display = "none";
+}
+
+menuBtn.addEventListener("click", () => {
+    showMenu()
+})
+
+menuBtnClose.addEventListener("click", () => {
+    hideMenu()
+})
+
+
+// Navigation
+
+const aboutBtn = document.getElementById("about__btn");
+const workBtn = document.getElementById("work__btn");
+const skillzBtn = document.getElementById("skillz__btn");
+
+const about = document.getElementById("about");
+const work = document.getElementById("my-work__watermark");
+const skillz = document.getElementById("my-skillz__watermark");
+
+aboutBtn.addEventListener("click", () => {
+    about.scrollIntoView({ behavior: "smooth" })
+    hideMenu()
+})
+
+workBtn.addEventListener("click", () => {
+    work.scrollIntoView({ behavior: "smooth" })
+    hideMenu()
+})
+
+skillzBtn.addEventListener("click", () => {
+    skillz.scrollIntoView({ behavior: "smooth" })
+    hideMenu()
 })
